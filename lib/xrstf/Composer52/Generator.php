725f15a4a49b01cf12cf597d9adb1baf9958d653
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-07-19 04:10:24 +0600

    Fix optimize autoloader guessing

--- lib/xrstf/Composer52/Generator.php
+++ lib/xrstf/Composer52/Generator.php
@@ -28,15 +28,8 @@ class Generator {
 		// it expects a full definition of the current command line and we can't
 		// provide that.
 
-//		$def   = new InputDefinition(array(new InputOption('optimize', 'o', InputOption::VALUE_NONE)));
-//		$input = new ArgvInput(null, $def);
-//		var_dump($input->hasOption('o')); // "Too many arguments"
-
-//		$options  = getopt('o', array('optimize-autoloader')); // always array()
-//		$optimize = !empty($options);
-
 		$args     = $_SERVER['argv'];
-		$optimize = in_array('-o', $args) || in_array('-o', $args);
+		$optimize = in_array('-o', $args) || in_array('--optimize-autoloader', $args) || in_array('--optimize', $args);
 
 		$generator = new AutoloadGenerator();
 		$generator->dump($config, $localRepo, $package, $installationManager, 'composer', $optimize);
