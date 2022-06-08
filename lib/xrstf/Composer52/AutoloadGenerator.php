3bd2a078a444412d02ca4b7fe3a95fd7495b2d4d
theminecoder <theminecoder@users.noreply.github.com>
2022-06-08 13:04:22 +1000

    Update to new getAutoloadRealFile signature

--- lib/xrstf/Composer52/AutoloadGenerator.php
+++ lib/xrstf/Composer52/AutoloadGenerator.php
@@ -228,7 +228,7 @@ return ComposerAutoloaderInit$suffix::getLoader();
 AUTOLOAD;
 	}
 
-	protected function getAutoloadRealFile($useClassMap, $useIncludePath, $targetDirLoader, $filesCode, $vendorPathCode, $appBaseDirCode, $suffix, $useGlobalIncludePath, $prependAutoloader, $staticPhpVersion = 70000) {
+	protected function getAutoloadRealFile($useClassMap, $useIncludePath, $targetDirLoader, $filesCode, $vendorPathCode, $appBaseDirCode, $suffix, $useGlobalIncludePath, $prependAutoloader, $staticPhpVersion = 70000, $checkPlatform = false) {
 		// TODO the class ComposerAutoloaderInit should be revert to a closure
 		// when APC has been fixed:
 		// - https://github.com/composer/composer/issues/959
